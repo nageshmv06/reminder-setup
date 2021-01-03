947762118c4ac2f908310a083ce7ca126955d2b5
nageshmv06 <nageshmv18@gmail.com>
2021-01-03 12:16:21 +0530

    second commit

---
+++ src/data.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    id: 1,
+    name: 'Bertie Yates',
+    age: 29,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
+  },
+  {
+    id: 2,
+    name: 'Hester Hogan',
+    age: 32,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
+  },
+  {
+    id: 3,
+    name: 'Larry Little',
+    age: 36,
+    image:
+      'https://www.adweek.com/wp-content/uploads/2018/07/confused-guy-meme-content-2018.jpg',
+  },
+  {
+    id: 4,
+    name: 'Xavier',
+    age: 34,
+    image:
+      'https://fnewshub.com/wp-content/uploads/2020/11/FB_IMG_1605666747087.jpg',
+  },
+  {
+    id: 5,
+    name: 'Lola Gardner',
+    age: 29,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+  },
+];
