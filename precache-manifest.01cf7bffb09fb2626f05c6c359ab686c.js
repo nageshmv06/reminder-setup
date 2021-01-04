db9aa93678822121d4a86c10adb8d8056e795e48
nageshmv06 <nageshmv18@gmail.com>
2021-01-04 23:21:14 +0530

    Updates

---
+++ precache-manifest.01cf7bffb09fb2626f05c6c359ab686c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53a019f5e01f6cdc305df74ce047b53a",
+    "url": "/reminder-setup/index.html"
+  },
+  {
+    "revision": "d63c346cb1148575effd",
+    "url": "/reminder-setup/static/css/main.45dd2827.chunk.css"
+  },
+  {
+    "revision": "b7e28d145fb5aaaf2503",
+    "url": "/reminder-setup/static/js/2.6929fade.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reminder-setup/static/js/2.6929fade.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d63c346cb1148575effd",
+    "url": "/reminder-setup/static/js/main.099131be.chunk.js"
+  },
+  {
+    "revision": "5ba4ba6a32d5bf85bebd",
+    "url": "/reminder-setup/static/js/runtime-main.063e2d01.js"
+  }
+]);
